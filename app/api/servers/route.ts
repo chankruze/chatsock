70a7b93b7e0fd4babe88efe30c5eec0a6182390a
chankruze <chankruze@gmail.com>
2023-08-27 02:17:17 -0400

    fix: restrict server invite code to 6 digits
    Signed-off-by: chankruze <chankruze@gmail.com>

--- app/api/servers/route.ts
+++ app/api/servers/route.ts
@@ -26,7 +26,7 @@ export async function POST(req: Request) {
         ownerId: profile.id,
         name,
         icon,
-        inviteCode: nanoid(),
+        inviteCode: nanoid(6),
         channels: {
           create: [{ name: 'general', creatorId: profile.id }],
         },
