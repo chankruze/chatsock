dc554b17390ad8b8d8c5e44ea493d2b6827e8988
chankruze <chankruze@gmail.com>
2023-08-28 07:11:04 -0400

    feat: implement edit and delete a channel functionality
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ components/modals/delete-channel-modal.tsx
@@ -0,0 +1,89 @@
+/*
+Author: chankruze (chankruze@gmail.com)
+Created: Mon Aug 28 2023 07:07:34 GMT-0400 (Eastern Daylight Time)
+
+Copyright (c) geekofia 2023 and beyond
+*/
+
+'use client';
+
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import qs from 'query-string';
+import { useState } from 'react';
+
+import { Button } from '@/components/ui/button';
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+} from '@/components/ui/dialog';
+import { useModal } from '@/hooks/use-modal-store';
+
+export const DeleteChannelModal = () => {
+  const { isOpen, onClose, type, data } = useModal();
+  const router = useRouter();
+
+  const isModalOpen = isOpen && type === 'deleteChannel';
+  const { server, channel } = data;
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+      const url = qs.stringifyUrl({
+        url: `/api/channels/${channel?.id}`,
+        query: {
+          serverId: server?.id,
+        },
+      });
+
+      await axios.delete(url);
+
+      onClose();
+      router.refresh();
+      router.push(`/servers/${server?.id}`);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent className="overflow-hidden bg-white p-6 text-black">
+        <DialogHeader>
+          <DialogTitle className="text-2xl font-bold capitalize">
+            Delete channel
+          </DialogTitle>
+          <DialogDescription className="text-zinc-500">
+            Are you sure you want to do this? <br />
+            <span className="font-semibold text-indigo-500">
+              #{channel?.name}
+            </span>{' '}
+            will be permanently deleted.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <div className="flex w-full items-center justify-between">
+            <Button disabled={isLoading} onClick={onClose} variant="secondary">
+              Cancel
+            </Button>
+            <Button
+              disabled={isLoading}
+              variant="destructive"
+              onClick={onClick}
+            >
+              Confirm
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -11,7 +11,9 @@ import { useEffect, useState } from 'react';
 
 import { CreateChannelModal } from '@/components/modals/create-channel-modal';
 import { CreateServerModal } from '@/components/modals/create-server-modal';
+import { DeleteChannelModal } from '@/components/modals/delete-channel-modal';
 import { DeleteServerModal } from '@/components/modals/delete-server-modal';
+import { EditChannelModal } from '@/components/modals/edit-channel-modal';
 import { EditServerModal } from '@/components/modals/edit-server-modal';
 import { LeaveServerModal } from '@/components/modals/leave-server-modal';
 import { ManageMembersModal } from '@/components/modals/manage-members-modal';
@@ -39,6 +41,8 @@ export const ModalProvider = () => {
       <CreateChannelModal />
       <DeleteServerModal />
       <LeaveServerModal />
+      <EditChannelModal />
+      <DeleteChannelModal />
     </>
   );
 };
